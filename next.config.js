d90ed035a7734c3e9abfaebbad2cb7021d8a58e6
mmarfinetz <mitchmarfinetz@gmail.com>
2025-02-21 12:28:00 -0500

    force: add build ID to force fresh builds

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // This will generate static HTML/CSS/JS
-  distDir: 'public', // This sets the output directory to 'public'
   images: {
     unoptimized: true // Required for static export
   },
   // Ensure we can still use the calculator route
   basePath: '',
   trailingSlash: true,
+  env: {
+    BUILD_ID: new Date().getTime().toString(),  // Force new build each time
+  },
 }
 
 module.exports = nextConfig 
